b48b8b862455a928f0f184cec935a6217eeae061
sahmadnakumbe-8 <sahmad nakumbe>
2026-09-08 22:40:57 -0500

    Add stylesheet and JavaScript

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const portfolio = document.getElementById("portfolio");
+
+gamesButton.addEventListener("click", function () {
+    alert("Welcome to portfolio!");
+});
+
+const gamesButton = document.getElementById("gamesButton");
+
+gamesButton.addEventListener("click", function () {
+    alert("Welcome to my games!");
+});
